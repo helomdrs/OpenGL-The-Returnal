e163e4cb4d43293e8e00c24d7d7ef049c2dfd78e
heloisa.medeiros <heloisam.silva@hotmail.com>
2024-10-14 15:09:58 -0300

    drawing a triangle

--- OpenGLTheReturnal/OpenGLTheReturnal/Source/Main.cpp
+++ OpenGLTheReturnal/OpenGLTheReturnal/Source/Main.cpp
@@ -1,18 +1,16 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <SDL.h>
-#include <SDL_opengl.h>
-#include <SFML/Window.hpp>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <SOIL2/SOIL2.h>
 
 #include <iostream>
 
+//Window dimensions
+const GLint WINDOW_WIDTH = 640, WINDOW_HEIGHT = 480;
+
+const GLchar* vertexShaderSource = "#version 330 core\nlayout(location = 0) in vec3 position;\nvoid main()\n{\ngl_Position = vec4(position, 1.0);\n}\n";
+const GLchar* fragmentShaderSource = "#version 330 core\nout vec4 color;\nvoid main()\n{\ncolor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n}\n";
+
 int main(void)
 {
-    GLFWwindow* glfwWindow;
-
     //Initialize GLFW library
     if (!glfwInit())
         return -1;
@@ -24,27 +22,15 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    //SDL initialization
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    //SDL OpenGL property settings
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8); //SDL buffer size
-
-    //SFML initialization and property settings
-    sf::ContextSettings sfmlSettings;
-    sfmlSettings.depthBits = 24;
-    sfmlSettings.stencilBits = 8;
-    sfmlSettings.majorVersion = 3;
-    sfmlSettings.minorVersion = 3;
-    sfmlSettings.attributeFlags = sf::ContextSettings::Core;
-
     // Create a GLFW windowed mode window and its OpenGL context
-    glfwWindow = glfwCreateWindow(640, 480, "Hello GLFW", NULL, NULL);
+    GLFWwindow *glfwWindow = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Hello GLFW", NULL, NULL);
+
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(glfwWindow, &screenWidth, &screenHeight);
+
     if (!glfwWindow)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
@@ -52,34 +38,100 @@ int main(void)
     // Make the window's context current
     glfwMakeContextCurrent(glfwWindow);
 
+    //Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+    glewExperimental = GL_TRUE;
+
     GLenum err = glewInit();
     if (GLEW_OK != err)
     {
         // Problem: glewInit failed, something is seriously wrong.
         std::cout << "Error: " << glewGetErrorString(err) << std::endl;
+        return -1;
     }
 
-    //Creates a SDL Window
-    SDL_Window* sdlWindow = SDL_CreateWindow("Hello SDL", 200, 200, 640, 480, SDL_WINDOW_OPENGL);
-
-    //Creates a context to add the window to it
-    SDL_GLContext sdlContext = SDL_GL_CreateContext(sdlWindow);
-
-    //Creates a SFML window
-    sf::Window sfmlWindow(sf::VideoMode(640, 480, 32), "Hello SFML", sf::Style::Default, sfmlSettings);
-
-    SDL_version sdlVersion{};
-    SDL_VERSION(&sdlVersion)
     std::cout << "Status: Using GL " << glGetString(GL_VERSION) << std::endl;
     std::cout << "Status: Using GLEW " << glewGetString(GLEW_VERSION) << std::endl;
-    std::cout << "Status: Using SDL " << (int)sdlVersion.major << "." << (int)sdlVersion.minor << "." << (int)sdlVersion.patch << std::endl;
-    std::cout << "Status: Using SFML 2.6.1" << std::endl;
+
+    //Define the viewport dimensions
+    glViewport(0, 0, screenWidth, screenHeight);
+
+    //Build vertex and fragmant shaders
+    GLint success;
+    GLchar infoLog[512];
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR: Vertex Shader compilation failed: " << infoLog << std::endl;
+    }
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR: Fragment Shader compilation failed: " << infoLog << std::endl;
+    }
+
+    //Shader program linking and compiling 
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR: Shader program linking failed: " << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    //Creating the vertex data to draw the triangle
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f, //bottom left
+         0.5f, -0.5f, 0.0f, //bottom right
+         0.0f,  0.5f, 0.0f  //top middle
+    };
+
+    //creating and binding vertex buffer and array objects
+    GLuint vertexBufferObject, vertexArrayObject;
+    glGenVertexArrays(1, &vertexArrayObject);
+    glGenBuffers(1, &vertexBufferObject);
+
+    glBindVertexArray(vertexArrayObject);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
     
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(glfwWindow))
     {
+        //Check if any input events have been activated and call corresponding response function
+        glfwPollEvents();
+
+
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vertexArrayObject);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(glfwWindow);
@@ -87,6 +139,11 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    //Cleanup
+    glDeleteVertexArrays(1, &vertexArrayObject);
+    glDeleteBuffers(1, &vertexBufferObject);
+
 
     glfwTerminate();
     return 0;
